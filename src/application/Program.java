7fc2bd5baa4fbf0cb4d4d7d8edd69661a5e4f704
Tiago Silva <tgsilva1991@gmail.com>
2022-05-18 16:13:46 +0100

    Project finished

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,36 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int students;
+		char ch = 'A';
+		
+		Set<Integer> set = new HashSet<>();
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print("How many students for course " + ch++ + "? ");
+			students = sc.nextInt(); sc.nextLine();
+			
+			for(int j = 0; j < students; j++) {
+				int studentNumber = sc.nextInt(); sc.nextLine();
+				set.add(studentNumber);
+			}
+		}
+		
+		System.out.print("Total students: " + set.size());
+		
+		
+		sc.close();
 	}
 
 }
